6c594af79b3d5232cb1da19b5623ffb1bc601b1c
Christian Kyle Autor <cautor3@gmail.com>
2025-08-29 16:54:33 +0800

    init

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Blog;
+
+class BlogController extends Controller
+{
+    Blog::create([
+        
+    ]);
+}
---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('users', function ($id) {
+        
+    });
+    
+    Route::get('users', function ($id) {
+        
+    });
+});
